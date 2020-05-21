9fc59b0a5d311ae0f3d2532126aa6dab13991ad5
perugu-4643 <perugu.goutham@zohocorp.com>
2020-05-22 01:10:11 +0530

    Added tests for DB insert and fetch

--- app/src/androidTest/java/com/perugu/goutham/newsapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/perugu/goutham/newsapp/ExampleInstrumentedTest.kt
@@ -1,24 +1,95 @@
 package com.perugu.goutham.newsapp
 
-import androidx.test.platform.app.InstrumentationRegistry
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.google.gson.Gson
+import com.perugu.goutham.newsapp.db.NewsDb
+import com.perugu.goutham.newsapp.repository.NetworkRequestRepository
+import com.perugu.goutham.newsapp.viewmodel.NewsViewModel
+import junit.framework.TestCase
+import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
+class InstrumentedTest {
+
+    private lateinit var newsViewModel: NewsViewModel
+
+    private lateinit var newsDb: NewsDb
+
+    @Before
+    fun setUp(){
+
+        val context = ApplicationProvider.getApplicationContext<Context>()
+
+        newsDb = Room.inMemoryDatabaseBuilder(
+            context,
+            NewsDb::class.java
+        ).build()
+
+        val networkRequestRepository = NetworkRequestRepository(
+            OkHttpClient(),
+            Gson(),
+            newsDb
+        )
+
+        newsViewModel = NewsViewModel(networkRequestRepository)
+        newsViewModel.setCoRoutineDispacher(Dispatchers.Unconfined)
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.perugu.goutham.newsapp", appContext.packageName)
+    fun insertAndFetchFromDbTest(){
+        newsViewModel.fetchNewsFeeds()
+        val value = getValue(newsViewModel.newsFeedsLiveData)
+        value.size shouldBe 10 //Asserting using infix helper that the total list size is 10 (as the n/w response return only top 10 headlines)
+
+        //Just printing all author names
+        value.forEach {
+            println(it.author)
+        }
+
+    }
+
+    @After
+    fun closeDb(){
+        newsDb.close()
+    }
+
+    @Throws(InterruptedException::class)
+    fun <T> getValue(liveData: LiveData<T>): T {
+        val data = arrayOfNulls<Any>(1)
+        val latch = CountDownLatch(1)
+        val observer = object : Observer<T> {
+            override fun onChanged(t: T?) {
+                data[0] = t
+                latch.countDown()
+                liveData.removeObserver(this)
+            }
+
+        }
+
+        Handler(Looper.getMainLooper()).post {
+            liveData.observeForever(observer)
+        }
+
+        latch.await(2, TimeUnit.SECONDS)
+
+        return data[0] as T
+    }
+
+    infix fun <T> T.shouldBe(any: Any?) {
+        TestCase.assertEquals(any, this)
     }
 }
--- app/src/main/java/com/perugu/goutham/newsapp/viewmodel/NewsViewModel.kt
+++ app/src/main/java/com/perugu/goutham/newsapp/viewmodel/NewsViewModel.kt
@@ -1,10 +1,12 @@
 package com.perugu.goutham.newsapp.viewmodel
 
+import androidx.annotation.VisibleForTesting
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.perugu.goutham.newsapp.Article
 import com.perugu.goutham.newsapp.repository.NetworkRequestRepository
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -12,13 +14,20 @@ class NewsViewModel(
     private val networkRequestRepository: NetworkRequestRepository
 ) : ViewModel() {
 
+    private var coroutineDispatcher = Dispatchers.IO
+
     val newsFeedsLiveData: LiveData<List<Article>>
     get() = networkRequestRepository.newsDb.newsFeedDao().getNewsFeeds()
 
     fun fetchNewsFeeds() {
-        viewModelScope.launch(Dispatchers.IO){
+        viewModelScope.launch(coroutineDispatcher){
             networkRequestRepository.fetchNewsFeeds()
         }
+    }
+
+    @VisibleForTesting
+    fun setCoRoutineDispacher(coroutineDispatcher: CoroutineDispatcher){
+        this.coroutineDispatcher = coroutineDispatcher
     }
 
 }

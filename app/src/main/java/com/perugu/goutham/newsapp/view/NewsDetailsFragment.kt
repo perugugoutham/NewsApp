0b831681f5414661330a3de1c924431229b8e26f
perugu-4643 <perugu.goutham@zohocorp.com>
2020-05-22 18:16:59 +0530

    News deatils fragment complete UI implementation.

--- app/src/main/java/com/perugu/goutham/newsapp/view/NewsDetailsFragment.kt
+++ app/src/main/java/com/perugu/goutham/newsapp/view/NewsDetailsFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.google.gson.Gson
 import com.perugu.goutham.newsapp.R
 import com.perugu.goutham.newsapp.dagger.NewsAppComponentProvider
 import com.perugu.goutham.newsapp.db.NewsDb
 import com.perugu.goutham.newsapp.viewmodel.NewsFeedViewModelFactory
 import com.perugu.goutham.newsapp.viewmodel.NewsViewModel
+import com.squareup.picasso.Picasso
 import okhttp3.OkHttpClient
+import java.text.SimpleDateFormat
+import java.util.*
 import javax.inject.Inject
 
 class NewsDetailsFragment: Fragment() {
@@ -27,6 +32,9 @@ class NewsDetailsFragment: Fragment() {
 
     @Inject
     lateinit var newsDatabase: NewsDb
+
+    @Inject
+    lateinit var picassoClient: Picasso
 
     private val newsViewModel: NewsViewModel by activityViewModels {
         NewsAppComponentProvider.getNewsAppComponent(this.requireContext()).inject(this)
@@ -43,8 +51,22 @@ class NewsDetailsFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        NewsAppComponentProvider.getNewsAppComponent(this.requireContext()).inject(this)
+
+        view.findViewById<ImageView>(R.id.back_button).setOnClickListener {
+            findNavController().popBackStack()
+        }
+
         newsViewModel.selectedArticle.observe(viewLifecycleOwner, Observer {
-            requireView().findViewById<TextView>(R.id.description).text = it.title
+            requireView().findViewById<TextView>(R.id.description).text = it.description
+            requireView().findViewById<TextView>(R.id.title).text = it.title
+            requireView().findViewById<TextView>(R.id.source).text = it.source.name
+
+            val simpleDateFormat = SimpleDateFormat("YYYY-MM-dd", Locale.US)
+            requireView().findViewById<TextView>(R.id.date).text = simpleDateFormat.format(it.publishedAt)
+
+            picassoClient.load(it.urlToImage).into(requireView().findViewById<ImageView>(R.id.thumbnail_image))
         })
     }
 
